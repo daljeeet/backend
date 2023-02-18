505fca0c9f17a8b7f5cc35c6af4acdc1347323e6
Daljeet Singh <80110392+daljeet-coder@users.noreply.github.com>
2023-02-18 12:37:14 +0530

    Update todo.middle.js

--- middleware/todo.middle.js
+++ middleware/todo.middle.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 require("dotenv").config()
 const crudAuth = async(req,res,next)=>{
     try{
-        let token = req.headers.authorization
+        let token = req.headers.authorization.split(" ")[1]
        let authId=jwt.decode(token,process.env.key).id
         if(authId){
             req.body={...req.body,author:authId}
