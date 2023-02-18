a0d8a6ec22590a9be94fec63df4618586eddb8e3
Daljeet Singh <80110392+daljeet-coder@users.noreply.github.com>
2023-02-18 12:13:28 +0530

    Update todo.middle.js

--- middleware/todo.middle.js
+++ middleware/todo.middle.js
@@ -3,15 +3,13 @@ require("dotenv").config()
 const crudAuth = async(req,res,next)=>{
     try{
         let token = req.headers.authorization
-        console.log(token)
-       // let authId=jwt.decode(token,process.env.key).id
-        res.send({"wait":"sfd;lkj"})
-//         if(authId){
-//             req.body={...req.body,author:authId}
-//             next()
-//         }else{
-//             res.status(400).send({"msg":"authencation required"})
-//         }
+       let authId=jwt.decode(token,process.env.key).id
+        if(authId){
+            req.body={...req.body,author:authId}
+            next()
+        }else{
+            res.status(400).send({"msg":"authencation required"})
+        }
     }catch(err){
         res.send({"msg":"something went wrong","err":err})
     }
